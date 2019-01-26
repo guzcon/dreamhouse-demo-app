a5ac5f807018bf17900c24f5d99a7e56e2d4e595
guzcon <anh@pixels.fi>
2019-01-26 23:00:12 +0200

    fix gitignore file

--- server.js
+++ server.js
@@ -3,18 +3,92 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
+
+const pg = require('pg');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/dreamhouse';
+
+if (process.env.DATABASE_URL !== undefined) {
+  pg.defaults.ssl = true;
+}
+
+const const = new pg.Client(connectionString);
+client.connect();
+
 // API calls
-app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
+
+const propertyTable = 'property__c';
+const favoriteTable = 'favorite__c';
+const brokerTable = 'broker__c';
+
+// setup the demo data if needed
+client.query('SELECT * FROM salesforce.broker__c', function(error, data) {
+  if (error !== null) {
+    client.query('SELECT * FROM broker__c', function(error, data) {
+      if (error !== null) {
+        console.log('Loading Demo Data...');
+        require('./db/demo.js')(client);
+        console.log('Done Loading Demo Data!');
+      }
+    });
+  }
+  else {
+    const schema = 'salesforce.';
+    propertyTable = schema + 'property__c';
+    favoriteTable = schema + 'favorite__c';
+    brokerTable = schema + 'broker__c';
+  }
 });
-app.post('/api/world', (req, res) => {
-  console.log(req.body);
-  res.send(
-    `I received your POST request. This is what you sent me: ${req.body.post}`,
-  );
+
+
+app.get('/property', function(req, res) {
+  client.query('SELECT * FROM ' + propertyTable, function(error, data) {
+    res.json(data.rows);
+  });
 });
+
+app.get('/property/:id', function(req, res) {
+  client.query('SELECT ' + propertyTable + '.*, ' + brokerTable + '.sfid AS broker__c_sfid, ' + brokerTable + '.name AS broker__c_name, ' + brokerTable + '.email__c AS broker__c_email__c, ' + brokerTable + '.phone__c AS broker__c_phone__c, ' + brokerTable + '.mobile_phone__c AS broker__c_mobile_phone__c, ' + brokerTable + '.title__c AS broker__c_title__c, ' + brokerTable + '.picture__c AS broker__c_picture__c FROM ' + propertyTable + ' INNER JOIN ' + brokerTable + ' ON ' + propertyTable + '.broker__c = ' + brokerTable + '.sfid WHERE ' + propertyTable + '.sfid = $1', [req.params.id], function(error, data) {
+    res.json(data.rows[0]);
+  });
+});
+
+
+app.get('/favorite', function(req, res) {
+  client.query('SELECT ' + propertyTable + '.*, ' + favoriteTable + '.sfid AS favorite__c_sfid FROM ' + propertyTable + ', ' + favoriteTable + ' WHERE ' + propertyTable + '.sfid = ' + favoriteTable + '.property__c', function(error, data) {
+    res.json(data.rows);
+  });
+});
+
+app.post('/favorite', function(req, res) {
+  client.query('INSERT INTO ' + favoriteTable + ' (property__c) VALUES ($1)', [req.body.property__c], function(error, data) {
+    res.json(data);
+  });
+});
+
+app.delete('/favorite/:sfid', function(req, res) {
+  client.query('DELETE FROM ' + favoriteTable + ' WHERE sfid = $1', [req.params.sfid], function(error, data) {
+    res.json(data);
+  });
+});
+
+
+app.get('/broker', function(req, res) {
+  client.query('SELECT * FROM ' + brokerTable, function(error, data) {
+    res.json(data.rows);
+  });
+});
+
+app.get('/broker/:sfid', function(req, res) {
+  client.query('SELECT * FROM ' + brokerTable + ' WHERE sfid = $1', [req.params.sfid], function(error, data) {
+    res.json(data.rows[0]);
+  });
+});
+
+// production build
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
   app.use(express.static(path.join(__dirname, 'client/build')));
